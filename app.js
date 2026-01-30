d4bc7179433640c0e865e80bc45faa906dc990ef
Enzo Moy <enzo.moy94@gmail.com>
2026-01-30 10:28:17 +0100

    Add database connection

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const initDatabase = require('./db.js')
 
 const indexRouter = require('./routes/index')
-//const usersRouter = require('./routes/users')
+const usersRouter = require('./routes/users')
 
 const app = express()
 
@@ -20,7 +24,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-//app.use('/users', usersRouter)
+app.use('/users', usersRouter)
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
